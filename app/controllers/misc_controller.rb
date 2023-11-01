0fadafcd4009837c5dfc672e726d1062bde0d503
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-11-01 05:16:37 +0000

    movie_actors

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -18,6 +18,22 @@ class MiscController < ApplicationController
     render({ :template => "misc_templates/dirflex"})
   end
 
+  def dir_youngest
+    @id = params.fetch("id")
+    @target = Director.all.where({ :id => @id}).first
+    @filmography = Movie.all.where({ :director_id => @id})
+
+    render({ :template => "misc_templates/diryoung"})
+  end
+
+  def dir_eldest
+    @id = params.fetch("id")
+    @target = Director.all.where({ :id => @id}).first
+    @filmography = Movie.all.where({ :director_id => @id})
+
+    render({ :template => "misc_templates/dirold"})
+  end
+
   def actors
     @acts = Actor.all
     @instances = Actor.count
@@ -25,10 +41,25 @@ class MiscController < ApplicationController
     render({ :template => "misc_templates/actors"})
   end
 
+  def act_specific
+    @id = params.fetch("id")
+    @target = Actor.all.where({ :id => @id}).first
+    @filmography = Character.all.where({ :actor_id => @id})
+
+    render({ :template => "misc_templates/actflex"})
+  end
+
   def movies
     @movs = Movie.all
     @instances = Movie.count
     
     render({ :template => "misc_templates/movies"})
+  end
+
+  def mov_specific
+    @id = params.fetch("id")
+    @target = Movie.all.where({ :id => @id}).first
+
+    render({ :template => "misc_templates/filmflex"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get "/directors", controller: "misc", action: "directors"
   get "directors/:id", controller: "misc", action: "dir_specific"
+  get "directors/youngest", controller: "misc", action: "dir_youngest"
+  get "directors/eldest", controller: "misc", action: "dir_eldest"
   get "/actors", controller: "misc", action: "actors"
+  get "actors/:id", controller: "misc", action: "act_specific"
   get "/movies", controller: "misc", action: "movies"
+  get "movies/:id", controller: "misc", action: "mov_specific"
 end
