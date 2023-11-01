863d9d2be1e8f09c5748dc28847ffab838d5e6eb
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-11-01 04:41:25 +0000

    directors

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,19 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def directors
+    @dirs = Director.all
+    @instances = Director.count
+    
+    render({ :template => "misc_templates/directors"})
+  end
+
+  def specific
+    @id = params.fetch("id")
+    @target = Director.all.where({ :id => @id}).first
+    @filmography = Movie.all.where({ :director_id => @id})
+
+    render({ :template => "misc_templates/dirflex"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get "/directors", controller: "misc", action: "directors"
+  get "directors/:id", controller: "misc", action: "specific"
 end
